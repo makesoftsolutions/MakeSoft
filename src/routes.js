4203d6d6bb692975d847e7224e21497fa63bf717
JoaoVictorFBarros <joaovfrancisco2001@gmail.com>
2023-04-25 14:12:35 -0300

    commit

--- src/routes.js
+++ src/routes.js
@@ -9,10 +9,11 @@ export const relativepath = "/makesoft_solutions"
 export default function Approutes(props){
     return(
         <Routes>
-            <Route path={relativepath} element={<Whoweare mobile={props.mobile}/>}/>
-            <Route path={relativepath + "/Objetivos"} element={<Goals mobile={props.mobile}/>}/>
-            <Route path={relativepath + "/servicos"} element={<Services mobile={props.mobile}/>}/>
-            <Route path={relativepath + "/Contato"} element={<Contactform mobile={props.mobile}/>}/>
+            <Route path="*" element={<Whoweare mobile={props.mobile}/>} redirect/>
+            <Route exact path={relativepath} element={<Whoweare mobile={props.mobile}/>}/>
+            <Route exact path={relativepath + "/Objetivos"} element={<Goals mobile={props.mobile}/>}/>
+            <Route exact path={relativepath + "/servicos"} element={<Services mobile={props.mobile}/>}/>
+            <Route exact path={relativepath + "/Contato"} element={<Contactform mobile={props.mobile}/>}/>
         </Routes>
     )
 }
