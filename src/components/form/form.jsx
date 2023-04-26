e01307c6e63a8890d8ec0a25a7587f3744292519
JoaoVictorFBarros <joaovfrancisco2001@gmail.com>
2023-04-25 21:46:32 -0300

    commit

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -3,10 +3,10 @@ export default function Contactform(props){
     let output
 
     if (props.mobile){
-        output = <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScLftno0h937iQIH2iN-wkth7sChx3cN20Ozsg1Gyb-uBfGPg/viewform?embedded=true" width="400em" height="1200em" frameborder="0" marginheight="0" marginwidth="0" title="Mobileform">Carregando…</iframe>
+        output = <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf8n2XHqAWfL9sbM0ekdHjqpJpB95ff99kNXSZyrb55KHqp2w/viewform?embedded=true"  width="400em" height="1200em" frameborder="0" marginheight="0" marginwidth="0" title="Mobileform">Carregando…</iframe>
     }
     else{
-        output = <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScLftno0h937iQIH2iN-wkth7sChx3cN20Ozsg1Gyb-uBfGPg/viewform?embedded=true" width="800em" height="1200em" frameborder="0" marginheight="0" marginwidth="0" title="form">Carregando…</iframe>
+        output = <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf8n2XHqAWfL9sbM0ekdHjqpJpB95ff99kNXSZyrb55KHqp2w/viewform?embedded=true"  width="800em" height="1200em" frameborder="0" marginheight="0" marginwidth="0" title="form">Carregando…</iframe>
     }
     
     return(
